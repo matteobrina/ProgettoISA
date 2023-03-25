3b55ab6a03cfbd3a09dee2272dadf87fd125bb33
Matteo Brina <matteo.brina@edu.unife.it>
2023-03-25 15:32:58 +0100

    Creazione file RUNME e collegamento db aws

--- mioProgetto/src/main/java/it/isa/progetto/Configuration.java
+++ mioProgetto/src/main/java/it/isa/progetto/Configuration.java
@@ -12,7 +12,7 @@ public class Configuration {
   public static final String DATABASE_DRIVER="com.mysql.cj.jdbc.Driver";
   public static final String SERVER_TIMEZONE=Calendar.getInstance().getTimeZone().getID();
   public static final String 
-    DATABASE_URL="jdbc:mysql://192.168.1.4:3306/PROGETTO2?user=prova&password=Internazionale99!&useSSL=true&serverTimezone="+SERVER_TIMEZONE;
+    DATABASE_URL="jdbc:mysql://35.159.38.87:3306/PROGETTO2?user=prova&password=Internazionale99!&useSSL=true&serverTimezone="+SERVER_TIMEZONE;
   
 }
   
