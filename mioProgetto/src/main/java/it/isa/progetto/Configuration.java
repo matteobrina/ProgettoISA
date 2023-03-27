bfca3045e539a681b27cfb1ce9bad9eece4a61d5
Matteo Brina <matteo.brina@edu.unife.it>
2023-03-27 11:02:45 +0200

    Commit definitivo docker

--- mioProgetto/src/main/java/it/isa/progetto/Configuration.java
+++ mioProgetto/src/main/java/it/isa/progetto/Configuration.java
@@ -12,7 +12,7 @@ public class Configuration {
   public static final String DATABASE_DRIVER="com.mysql.cj.jdbc.Driver";
   public static final String SERVER_TIMEZONE=Calendar.getInstance().getTimeZone().getID();
   public static final String 
-    DATABASE_URL="jdbc:mysql://35.159.38.87:3306/PROGETTO2?user=prova&password=Internazionale99!&useSSL=true&serverTimezone="+SERVER_TIMEZONE;
+    DATABASE_URL="jdbc:mysql://ec2-18-157-166-237.eu-central-1.compute.amazonaws.com:3306/PROGETTO2?user=prova&password=Internazionale99!&useSSL=true&serverTimezone="+SERVER_TIMEZONE;
   
 }
   
